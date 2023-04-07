75659bb4a43c9783e7befa660aef00a38dcf5596
LinusHPEnvy <LinusHPEnvy@Badonkers.fritz.box>
2023-04-07 17:45:57 +0200

    Updated registers: now a class

--- PIC-Simulator/src/pic16f84_simulator/MicroController.java
+++ PIC-Simulator/src/pic16f84_simulator/MicroController.java
@@ -1,16 +1,34 @@
 package pic16f84_simulator;
 
+import pic16f84_simulator.backend.UnknownLocationException;
 import pic16f84_simulator.backend.memory.Program_Memory;
 import pic16f84_simulator.backend.memory.RAM_Memory;
+import pic16f84_simulator.backend.memory.Reg;
+import pic16f84_simulator.backend.memory.Register;
 
 public class MicroController {
+    
     private Program_Memory pm;
     private RAM_Memory ram;
-    private int[] instrReg;
+    
+    public static Register instrReg;
     
     MicroController() {
         this.pm = new Program_Memory();
         this.ram = new RAM_Memory();
-        this.instrReg = new int[16];
+        
+        MicroController.instrReg = new Register(14);
     }
+    
+    
+    
+    // unnötig falls Register static
+//    public Register getReg(Reg reg) {        
+//        switch(reg) {
+//        case INSTR -> { return this.instrReg;}
+//        default -> { throw new UnknownLocationException("This Register is not known");}
+//        }
+//    }
+    
+    
 }
---
+++ PIC-Simulator/src/pic16f84_simulator/backend/memory/Reg.java
@@ -0,0 +1,7 @@
+package pic16f84_simulator.backend.memory;
+
+public enum Reg {
+    
+    INSTR, W
+
+}
