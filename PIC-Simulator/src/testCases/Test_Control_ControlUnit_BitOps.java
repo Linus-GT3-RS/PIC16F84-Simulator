0dc56ac1006123d9dfa63278fbbb8c9cb04b4f23
eduar <eduar@141.79.45.254>
2023-04-20 10:25:36 +0200

    fix

--- PIC-Simulator/src/testCases/Test_Control_ControlUnit_BitOps.java
+++ PIC-Simulator/src/testCases/Test_Control_ControlUnit_BitOps.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import pic16f84_simulator.MicroC;
+
 class Test_Control_ControlUnit_BitOps {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testBCF() {
+        MicroC mc = new MicroC();
+        mc.pm.writeSpecificBit(15, 3, 0); // 01 00
+        mc.pm.
+        mc.cu.exe();
     }
 
 }
