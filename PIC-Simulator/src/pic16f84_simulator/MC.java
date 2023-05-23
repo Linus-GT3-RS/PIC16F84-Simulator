ee741db5eb596eb321b4882206ba14af3b85aa28
Linus <dotexelhd@gmail.com>
2023-05-23 22:34:56 +0200

    added stackVariable to MC

--- PIC-Simulator/src/pic16f84_simulator/MC.java
+++ PIC-Simulator/src/pic16f84_simulator/MC.java
@@ -4,6 +4,7 @@ import pic16f84_simulator.backend.calculation.ALU;
 import pic16f84_simulator.backend.control.ControlUnit;
 import pic16f84_simulator.backend.memory.Program_Memory;
 import pic16f84_simulator.backend.memory.RAM_Memory;
+import pic16f84_simulator.backend.memory.StackBuffer;
 import pic16f84_simulator.backend.tools.Utils;
 
 public class MC {
@@ -15,6 +16,7 @@ public class MC {
     
     public static Program_Memory pm = new Program_Memory();
     public static RAM_Memory ram = new RAM_Memory();
+    public static StackBuffer stack = new StackBuffer();
     
     public static ControlUnit control = new ControlUnit();
     public static ALU alu = new ALU();
