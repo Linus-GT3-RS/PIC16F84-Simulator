b1ddda4f9d5c8367fe17dacc3f7d25a3fd857c51
Linus <dotexelhd@gmail.com>
2023-05-31 00:11:31 +0200

    Layout for Interrupts and Reset

---
+++ PIC-Simulator/src/pic16f84_simulator/backend/control/Interrupts.java
@@ -0,0 +1,9 @@
+package pic16f84_simulator.backend.control;
+
+public class Interrupts {
+    
+    // TMR0 Interrupt Linus
+    
+    // WTDOG abgelaufenInterrupt -> Reset Eduard
+
+}
---
+++ PIC-Simulator/src/pic16f84_simulator/backend/control/Reset.java
@@ -0,0 +1,9 @@
+package pic16f84_simulator.backend.control;
+
+public class Reset {
+    
+    // PowerOnReset Linus
+    
+    // OtherReset Eduard
+
+}
