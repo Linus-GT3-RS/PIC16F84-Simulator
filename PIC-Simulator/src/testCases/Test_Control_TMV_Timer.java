d3dd215c67dbe7a77503d3a6eac182a6afe41e31
Linus <dotexelhd@gmail.com>
2023-05-31 12:58:45 +0200

    added testClass for Timer

---
+++ PIC-Simulator/src/testCases/Test_Control_TMV_Timer.java
@@ -0,0 +1,12 @@
+package testCases;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class Test_Control_TMV_Timer {
+
+    @Test
+    void testDelayAfterWritingTMR0() {} // is tested in Test_RAM: testCheckUp(): case1 (tmr0)
+    
+
+}
