e317cbdec090c64086b6bfe1ef0895ff85d17591
Linus <dotexelhd@gmail.com>
2023-05-31 22:00:49 +0200

    implemented Interrupts: stdResponseRoutine()

--- PIC-Simulator/src/pic16f84_simulator/backend/control/Interrupts.java
+++ PIC-Simulator/src/pic16f84_simulator/backend/control/Interrupts.java
@@ -1,8 +1,16 @@
 package pic16f84_simulator.backend.control;
 
-public class Interrupts {
+import pic16f84_simulator.MC;
+import pic16f84_simulator.backend.memory.SFR;
+
+public abstract class Interrupts {
     
-    // TMR0 Interrupt Linus
+    public static void stdResponseRoutine() {
+        SFR.setGIE(0);
+        MC.stack.push(-1);
+        MC.control.pc = 4;
+    }
+    
     
     // WTDOG abgelaufenInterrupt -> Reset Eduard
     void WatchDogReset(){
